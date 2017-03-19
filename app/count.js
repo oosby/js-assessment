d0c9262f3bc1c706459084860ab2780a16a4b895
olivia osby <olivia.osby@gmail.com>
2017-03-19 16:03:36 -0400

    recursion and timers

--- app/count.js
+++ app/count.js
@@ -2,6 +2,19 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    const countTimer = setInterval(startTimer, 100);
+    let num = start;
 
+    function cancel() {
+      clearInterval(countTimer);
+    };
+
+    function startTimer() {
+      console.log(num);
+      (num === end) && cancel() || num++;
+    }
+
+    startTimer();
+    return { cancel };
   }
 };
--- app/recursion.js
+++ app/recursion.js
@@ -34,14 +34,66 @@ exports.recursionAnswers = {
   },
 
   permute: function(arr) {
+    // Heap's algo
+    var result = [];
 
+    function swap(a, b, arr) {
+      const ioa = arr.indexOf(a);
+      const iob = arr.indexOf(b);
+      arr[ioa] = b;
+      arr[iob] = a;
+    }
+
+    function gen(len) {
+      if (len === 1) {
+        result.push(arr.concat());
+      } else {
+        const l = len - 1;
+        const even = len%2 === 0;
+
+        for(let i = 0; i < len; i++) {
+          gen(l);
+          swap((even) ? arr[i] : arr[0], arr[l], arr);
+        }
+      }
+    }
+
+    gen(arr.length);
+    return result;
   },
 
   fibonacci: function(n) {
+    function * fiboGen(max) {
+      let i = 0;
+      let prev = 0;
+      let prevprev = 0;
+      let res;
+      while (i < max) {
+        if (i < 2) {
+          prev = 1;
+          yield i;
+        }
 
+        res = prev + prevprev;
+        yield res;
+        prevprev = prev;
+        prev = res;
+        i++;
+      }
+    }
+    const it = fiboGen(n);
+    let i = 0;
+    let result;
+    while (i <= n) {
+      result = it.next();
+      i++;
+    }
+    return result.value;
   },
 
   validParentheses: function(n) {
-
+    console.log(n)
+    
+    // return result;
   }
 };
